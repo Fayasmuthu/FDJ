93659d6893211afe86a62446ce6cc94fe21d624e
Fayasmuthu <fayasmuthu45@gmail.com>
2023-09-24 21:54:27 -0700

    up1

--- web/urls.py
+++ web/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('',views.home_dark,name="home-dark"),
     path('home',views.index,name="index"),
     path('course',views.course,name="course"),
-    path('course-Carousel',views.course_carousel,name="course-carousel"),
+    path('blog',views.blog,name="blog"),
+    path('gallery',views.gallery,name="gallery"),
+    path('product',views.product,name="product"),
+    path('team',views.team,name="team"),
 ]
--- web/views.py
+++ web/views.py
@@ -12,3 +12,13 @@ def course_carousel(request):
 
 def home_dark(request):
     return render(request,("details/index-dark.html"))
+def blog(request):
+    return render(request,("details/blog.html"))
+def product(request):
+    return render(request,("details/product.html"))
+def team(request):
+    return render(request,("details/team.html"))
+def gallery(request):
+    return render(request,("details/gallery.html"))
+def contact(request):
+    return render(request,("details/contact.html"))
